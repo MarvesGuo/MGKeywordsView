2de047f5d9940cb3f1e8f00d38ff940847f7442f
虔灵 <guoqianling@haodf.com>
2016-03-11 10:31:07 +0800

    initialize application

---
+++ MGKeywordsView/MGKeywordsView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MGKeywordsView
+//
+//  Created by Marves Guo on 16/3/11.
+//  Copyright © 2016年 Marves Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MGKeywordsView/MGKeywordsView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MGKeywordsView
+//
+//  Created by Marves Guo on 16/3/11.
+//  Copyright © 2016年 Marves Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
